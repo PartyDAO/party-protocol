702fa65cd9934c3e54ead4730c2e249abac2098b
Nina Barbakadze <barbakadzeninaa@gmail.com>
2022-05-12 14:13:20 +0200

    refactor

--- contracts/crowdfund/GateKeeper.sol
+++ contracts/crowdfund/GateKeeper.sol
@@ -4,27 +4,39 @@ pragma solidity ^0.8;
 // import "./IGateKeeper.sol";
 import "../tokens/IERC20.sol";
 
+//able to flex to different types of gatekeeping logic
+//bytes calldata encoded 
+//based on type of gatekeeping
+//decode the data you expect
+//which should be token address and minimum balance
+
 /**
  * @notice defines whether or not a user can access party
  */
 contract GateKeeper {
+
+    address tokenAddress;
+    uint256 minimumBalance;
+
+  constructor (bytes memory _callData) {
+    
+  }
+
     /**
      * @notice defines whether or not a user can access party
-     * @param  participant contributor address
-     * @param minimumBalance minimum balance allowed for participation
-     * @param tokenAddress ERC-20 token address
-     * @param id user id
-     * @param userData optional arb data that verifies proof of membership
+     * @param _participant contributor address
+     * @param _id user id
+     * @param _userData optional arb data that verifies proof of membership
      * @return bool true of false depeing if the user has enough tokens
      */
     function isAllowed(
-        address participant,
-        uint256 minimumBalance,
-        address tokenAddress,
-        bytes12 id,
-        bytes memory userData
+        address _participant,
+        // uint256 _minimumBalance,
+        // address _tokenAddress,
+        bytes12 _id,
+        bytes memory _userData
     ) external view returns (bool) {
-        if (IERC20(tokenAddress).balanceOf(participant) >= minimumBalance) {
+        if (IERC20(tokenAddress).balanceOf(_participant) >= minimumBalance) {
             return true;
         } else {
             return false;
