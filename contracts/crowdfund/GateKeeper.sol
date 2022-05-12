d2a0c10d1a9534123221fa9ae29978ccb7f0beae
Nina Barbakadze <barbakadzeninaa@gmail.com>
2022-05-12 12:34:54 +0200

    fixed indentation

--- contracts/crowdfund/GateKeeper.sol
+++ contracts/crowdfund/GateKeeper.sol
@@ -1,30 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8;
 
-// import "./IGateKeeper.sol"; 
+// import "./IGateKeeper.sol";
 import "../tokens/IERC20.sol";
 
- /**
+/**
  * @notice defines whether or not a user can access party
  */
 contract GateKeeper {
-
-
-   /**
-   * @notice defines whether or not a user can access party
-   * @param  participant contributor address
-   * @param minimumBalance minimum balance allowed for participation
-   * @param tokenAddress ERC-20 token address 
-   * @param id user id
-   * @param userData optional arb data that verifies proof of membership 
-   * @return bool true of false depeing if the user has enough tokens 
-   */
+    /**
+     * @notice defines whether or not a user can access party
+     * @param  participant contributor address
+     * @param minimumBalance minimum balance allowed for participation
+     * @param tokenAddress ERC-20 token address
+     * @param id user id
+     * @param userData optional arb data that verifies proof of membership
+     * @return bool true of false depeing if the user has enough tokens
+     */
     function isAllowed(
-     address participant,
-     uint256 minimumBalance,
-     address tokenAddress,
-     bytes12 id,
-     bytes memory userData
+        address participant,
+        uint256 minimumBalance,
+        address tokenAddress,
+        bytes12 id,
+        bytes memory userData
     ) external view returns (bool) {
         if (IERC20(tokenAddress).balanceOf(participant) >= minimumBalance) {
             return true;
