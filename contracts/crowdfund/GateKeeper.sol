a2214df5004a1e26d9f0cb1224673b7287f5c305
Nina Barbakadze <barbakadzeninaa@gmail.com>
2022-05-12 11:58:28 +0200

    added ternary checking balance on provided token address

--- contracts/crowdfund/GateKeeper.sol
+++ contracts/crowdfund/GateKeeper.sol
@@ -1,10 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8;
 
-contract GateKeeper is IGateKeeper {
-    
-    function isAllowed(address participant, bytes12 id, bytes memory userData) external view returns (bool) {
+// import "./IGateKeeper.sol"; 
+import "../tokens/IERC20.sol";
 
+ /**
+ * @notice defines whether or not a user can access party
+ */
+contract GateKeeper {
+
+
+   /**
+   * @notice defines whether or not a user can access party
+   * @param  participant contributor address
+   * @param minimumBalance minimum balance allowed for participation
+   * @param tokenAddress ERC-20 token address 
+   * @param id user id
+   * @param userData optional arb data that verifies proof of membership 
+   * @return bool true of false depeing if the user has enough tokens 
+   */
+    function isAllowed(
+     address participant,
+     uint256 minimumBalance,
+     address tokenAddress,
+     bytes12 id,
+     bytes memory userData
+    ) external view returns (bool) {
+        if (IERC20(tokenAddress).balanceOf(participant) >= minimumBalance) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
 }
