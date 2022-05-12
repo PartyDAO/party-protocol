aca97f610708fc5911b018d7d807101678cc1e4f
Nina Barbakadze <barbakadzeninaa@gmail.com>
2022-05-12 09:54:30 +0200

    initial contract and function creation

---
+++ contracts/crowdfund/GateKeeper.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8;
+
+contract GateKeeper is IGateKeeper {
+    
+    function isAllowed(address participant, bytes12 id, bytes memory userData) external view returns (bool) {
+
+    }
+
+}
