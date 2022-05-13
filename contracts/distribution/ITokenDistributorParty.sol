61e81172f5b9d88baa145238c4afefb59dd9318e
Lawrence Forman <me@merklejerk.com>
2022-05-13 01:15:48 -0400

    Add a real license type

--- contracts/distribution/ITokenDistributorParty.sol
+++ contracts/distribution/ITokenDistributorParty.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8;
 
 // Interface the caller of `ITokenDistributor.createDistribution()` must implement.
--- contracts/proposals/EIP1271Callback.sol
+++ contracts/proposals/EIP1271Callback.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8;
 
 // Implements a configurable a EIP-1271 callback.
