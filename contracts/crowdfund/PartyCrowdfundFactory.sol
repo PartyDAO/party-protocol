31e79041a4f4f613fb8e68b2e99c6245928d20e4
Lawrence Forman <me@merklejerk.com>
2022-05-13 12:51:57 -0400

    add optional gate initialization in PartyCrowdfundFactory

--- contracts/crowdfund/PartyCrowdfundFactory.sol
+++ contracts/crowdfund/PartyCrowdfundFactory.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8;
 
 import "../globals/IGlobals.sol";
+import "../utils/LibRawResult.sol";
 
 import "./PartyBid.sol";
 import "./PartyBuy.sol";
@@ -9,6 +10,8 @@ import "./PartyCollectionBuy.sol";
 import "./PartyCrowdfundProxy.sol";
 
 contract PartyCrowdfundFactory {
+    using LibRawResult for bytes;
+
     event PartyBuyCreated(PartyBuy.PartyBuyOptions opts);
     event PartyBidCreated(PartyBid.PartyBidOptions opts);
     event PartyCollectionBuyCreated(PartyCollectionBuy.PartyCollectionBuyOptions opts);
@@ -30,11 +33,19 @@ contract PartyCrowdfundFactory {
         _PARTY_COLLECTION_BUY_IMPL_GLOBAL_KEY = partyCollectionBuyImplGlobalKey;
     }
 
-    function createPartyBuy(PartyBuy.PartyBuyOptions calldata opts)
-        external
+    function createPartyBuy(
+        PartyBuy.PartyBuyOptions memory opts,
+        bytes memory createGateCallData
+    )
+        public
         payable
         returns (PartyBuy inst)
     {
+        opts.gateKeeperId = _prepareGate(
+            opts.gateKeeper,
+            opts.gateKeeperId,
+            createGateCallData
+        );
         inst = PartyBuy(payable(new PartyCrowdfundProxy{ value: msg.value }(
             _GLOBALS,
             _PARTY_BUY_IMPL_GLOBAL_KEY,
@@ -43,11 +54,19 @@ contract PartyCrowdfundFactory {
         emit PartyBuyCreated(opts);
     }
 
-    function createPartyBid(PartyBid.PartyBidOptions calldata opts)
-        external
+    function createPartyBid(
+        PartyBid.PartyBidOptions memory opts,
+        bytes memory createGateCallData
+    )
+        public
         payable
         returns (PartyBid inst)
     {
+        opts.gateKeeperId = _prepareGate(
+            opts.gateKeeper,
+            opts.gateKeeperId,
+            createGateCallData
+        );
         inst = PartyBid(payable(new PartyCrowdfundProxy{ value: msg.value }(
             _GLOBALS,
             _PARTY_BID_IMPL_GLOBAL_KEY,
@@ -57,17 +76,48 @@ contract PartyCrowdfundFactory {
     }
 
     function createPartyCollectionBuy(
-        PartyCollectionBuy.PartyCollectionBuyOptions calldata opts
+        PartyCollectionBuy.PartyCollectionBuyOptions memory opts,
+        bytes memory createGateCallData
     )
-        external
+        public
         payable
         returns (PartyCollectionBuy inst)
     {
+        opts.gateKeeperId = _prepareGate(
+            opts.gateKeeper,
+            opts.gateKeeperId,
+            createGateCallData
+        );
         inst = PartyCollectionBuy(payable(new PartyCrowdfundProxy{ value: msg.value }(
             _GLOBALS,
             _PARTY_COLLECTION_BUY_IMPL_GLOBAL_KEY,
             abi.encode(opts)
         )));
         emit PartyCollectionBuyCreated(opts);
+    }
+
+    function _prepareGate(
+        IGateKeeper gateKeeper,
+        bytes12 gateKeeperId,
+        bytes memory createGateCallData
+    )
+        private
+        returns (bytes12 newGateKeeperId)
+    {
+        if (
+            address(gateKeeper) == address(0) ||
+            gateKeeperId != bytes12(0)
+        ) {
+            // Using an existing gate on the gatekeeper
+            // or not using a gate at all.
+            return gateKeeperId;
+        }
+        // Call the gate creation function on the gatekeeper.
+        (bool s, bytes memory r) = address(gateKeeper).call(createGateCallData);
+        if (!s) {
+            r.rawRevert();
+        }
+        // Result is always a bytes12.
+        return abi.decode(r, (bytes12));
     }
 }
