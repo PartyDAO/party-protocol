e552bc3189fb320efde90494b86adc9a429f90e3
Steve Klebanoff <steve.klebanoff@gmail.com>
2022-05-17 12:58:05 -0700

    working wip

--- sol-tests/distribution/TokenDistributor.t.sol
+++ sol-tests/distribution/TokenDistributor.t.sol
@@ -22,8 +22,7 @@ contract TokenDistributorTest is Test, TestUtils {
     globals = new Globals(ADMIN_ADDRESS);
     vm.prank(ADMIN_ADDRESS);
     globals.setAddress(LibGlobals.GLOBAL_DAO_WALLET, ADMIN_ADDRESS);
-    // TODO: why had to do this? getting weird error
-    distributor = TokenDistributor(payable(address(new TokenDistributor(globals))));
+    distributor = new TokenDistributor(globals);
   }
   
   function testEthDistributionSimple() public {
@@ -39,25 +38,19 @@ contract TokenDistributorTest is Test, TestUtils {
     distributor.partyDaoClaim(ds, DISTRIBUTION_ADDRESS);
     assertEq(DISTRIBUTION_ADDRESS.balance, 0.033425 ether);
     
-    vm.deal(address(3), 5 ether);
+    vm.deal(address(3), 100 ether);
     dummyParty.setOwner(address(3), 3);
     dummyParty.setShare(3, 0.34 ether);
     
-    // 0.4432155
+    vm.deal(address(4), 100 ether);
+    dummyParty.setOwner(address(4), 4);
+    dummyParty.setShare(4, 0.66 ether);
     
-    console.log(address(3).balance);
-    uint256 startGas = gasleft();
-    vm.prank(address(3));
-    distributor.claim(ds, 3, payable(address(3)));
-    uint256 endGas = gasleft();
+    uint256 ethGained1 = _getEthGained(ds, address(3), 3, address(3));
+    assertApproxEqAbs(ethGained1, 0.4432155 ether, 0.0000000000001 ether); // weird rounding error
     
-    // uint256 gasUsed = endGas - startGas;
-    // console.log(gasUsed);
-    // console.log(address(3).balance);
-    
-    
-    
-    // TODO: need to check ownerOf?
+    uint256 ethGained2 = _getEthGained(ds, address(4), 4, address(4));
+    assertApproxEqAbs(ethGained2, 0.8603595 ether, 0.0000000000001 ether); // weird rounding error
   }
   
   // TODO: emergency fns?
@@ -67,4 +60,23 @@ contract TokenDistributorTest is Test, TestUtils {
   // TODO: multiple distributions
   
   // TODO: what happens if called with zero amount?
+  
+  // TODO: check that cant claim multiple
+  
+  function _getEthGained(
+    TokenDistributor.DistributionInfo memory ds,
+    address prankAs,
+    uint256 tokenId,
+    address recipient
+  ) private returns (uint256) {
+    uint256 initialEth = recipient.balance;
+    uint256 startGas = gasleft();
+    vm.prank(prankAs);
+    distributor.claim(ds, tokenId, payable(recipient));
+    uint256 endGas = gasleft();
+    uint256 gasUsed = startGas - endGas;
+    uint256 newBalance = recipient.balance;
+    uint256 ethGained = newBalance - (initialEth + gasUsed);
+    return ethGained;
+  }
 }
