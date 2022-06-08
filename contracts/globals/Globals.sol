db9024771ce4ef9af993b3be0470e6719ec88182
Lawrence Forman <me@merklejerk.com>
2022-06-08 13:12:20 -0400

    add support for GLOBAL_DAO_AUTHORITIES set inclusion check

--- contracts/globals/Globals.sol
+++ contracts/globals/Globals.sol
@@ -6,7 +6,10 @@ import "./IGlobals.sol";
 // TODO: create2 upgradeable? 😉
 contract Globals is IGlobals {
     address public immutable MULTISIG;
+    // key -> word value
     mapping(uint256 => bytes32) private _wordValues;
+    // key -> word value -> isIncluded
+    mapping(uint256 => mapping(bytes32 => bool)) private _includedWordValues;
 
     error OnlyMultisigError();
 
@@ -21,23 +24,55 @@ contract Globals is IGlobals {
         MULTISIG = multiSig;
     }
 
-    function getAddress(uint256 id) external view returns (address) {
-        return address(uint160(uint256(_wordValues[id])));
+    function getBytes32(uint256 key) external view returns (bytes32) {
+        return _wordValues[key];
     }
 
-    function getImplementation(uint256 id) external view returns (Implementation) {
-        return Implementation(address(uint160(uint256(_wordValues[id]))));
+    function getUint256(uint256 key) external view returns (uint256) {
+        return uint256(_wordValues[key]);
     }
 
-    function getUint256(uint256 id) external view returns (uint256) {
-        return uint256(_wordValues[id]);
+    function getAddress(uint256 key) external view returns (address) {
+        return address(uint160(uint256(_wordValues[key])));
     }
 
-    function setAddress(uint256 id, address value) external onlyMultisig {
-        _wordValues[id] = bytes32(uint256(uint160(value)));
+    function getIncludesBytes32(uint256 key, bytes32 value) external view returns (bool) {
+        return _includedWordValues[key][value];
     }
 
-    function setUint256(uint256 id, uint256 value) external onlyMultisig {
-        _wordValues[id] = bytes32(value);
+    function getIncludesUint256(uint256 key, uint256 value) external view returns (bool) {
+        return _includedWordValues[key][bytes32(value)];
+    }
+
+    function getIncludesAddress(uint256 key, address value) external view returns (bool) {
+        return _includedWordValues[key][bytes32(uint256(uint160(value)))];
+    }
+
+    function setBytes32(uint256 key, bytes32 value) external onlyMultisig {
+        _wordValues[key] = value;
+    }
+
+    function getImplementation(uint256 key) external view returns (Implementation) {
+        return Implementation(address(uint160(uint256(_wordValues[key]))));
+    }
+
+    function setUint256(uint256 key, uint256 value) external onlyMultisig {
+        _wordValues[key] = bytes32(value);
+    }
+
+    function setAddress(uint256 key, address value) external onlyMultisig {
+        _wordValues[key] = bytes32(uint256(uint160(value)));
+    }
+
+    function setIncludesBytes32(uint256 key, bytes32 value, bool isIncluded) external onlyMultisig {
+        _includedWordValues[key][value] = isIncluded;
+    }
+
+    function setIncludesUint256(uint256 key, uint256 value, bool isIncluded) external onlyMultisig {
+        _includedWordValues[key][bytes32(value)] = isIncluded;
+    }
+
+    function setIncludesAddress(uint256 key, address value, bool isIncluded) external onlyMultisig {
+        _includedWordValues[key][bytes32(uint256(uint160(value)))] = isIncluded;
     }
 }
--- contracts/globals/LibGlobals.sol
+++ contracts/globals/LibGlobals.sol
@@ -17,4 +17,5 @@ library LibGlobals {
     uint256 internal constant GLOBAL_DAO_DISTRIBUTION_SPLIT     = 11;
     uint256 internal constant GLOBAL_DAO_WALLET                 = 12;
     uint256 internal constant GLOBAL_TOKEN_DISTRIBUTOR          = 13;
+    uint256 internal constant GLOBAL_DAO_AUTHORITIES            = 14;
 }
