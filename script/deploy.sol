e015dfe5a101c52f9589b87419df88c06d6c60d4
Marcus Molchany <marcusmolchany@gmail.com>
2022-06-08 11:48:22 -0600

    update deploy workflow

--- script/deploy.sol
+++ script/deploy.sol
@@ -6,44 +6,119 @@ import 'forge-std/Test.sol';
 // import '../contracts/crowdfund/PartyCrowdfundFactory.sol';
 import '../contracts/distribution/TokenDistributor.sol';
 import '../contracts/globals/Globals.sol';
+import '../contracts/globals/LibGlobals.sol';
 import '../contracts/party/PartyFactory.sol';
+import '../contracts/party/Party.sol';
+import '../contracts/proposals/ProposalExecutionEngine.sol';
+import '../contracts/proposals/opensea/SharedWyvernV2Maker.sol';
 
 contract Deploy is Test {
 
+  // TODO: verify these constants
   // constants
-  address partydaoMultisig = 0xF7f52Dd34bc21eDA08c0b804C7c1dbc48375820f;
+  address OPENSEA_EXCHANGE_ADDRESS = 0x7f268357A8c2552623316e2562D90e642bB538E5;
+  address PARTY_DAO_MULTISIG = 0xF7f52Dd34bc21eDA08c0b804C7c1dbc48375820f;
+  uint256 PARTY_DAO_DISTRIBUTION_SPLIT_BPS = 250;
+  uint256 OS_ZORA_AUCTION_DURATION = 86400; // 86400 seconds = 24 hours
+  address ZORA_AUCTION_HOUSE_ADDRESS = 0xE468cE99444174Bd3bBBEd09209577d25D1ad673;
 
   // temporary variables to store deployed contract addresses
   // PartyCrowdfundFactory partyCrowdfundFactoryAddress;
-  PartyFactory partyFactoryAddress;
-  TokenDistributor tokenDistributorAddress;
   Globals globals;
+  IWyvernExchangeV2 openseaExchange;
+  IZoraAuctionHouse zoraAuctionHouse;
+  Party partyImpl;
+  PartyFactory partyFactory;
+  ProposalExecutionEngine proposalEngineImpl;
+  SharedWyvernV2Maker sharedWyvernV2Maker;
+  TokenDistributor tokenDistributor;
 
   function run() public {
     console.log('Starting deploy script.');
     vm.startBroadcast();
 
+
     // DEPLOY_GLOBALS
+    console.log('');
     console.log('Deploying - Globals');
-    globals = new Globals(partydaoMultisig);
+    globals = new Globals(PARTY_DAO_MULTISIG);
     console.log('Deployed - Globals', address(globals));
 
-    // DEPLOY_PARTY_FACTORY
-    console.log('Deploying - PartyFactory');
-    partyFactoryAddress = new PartyFactory(globals);
-    console.log('Deployed - PartyFactory', address(partyFactoryAddress));
+    console.log('  Globals - setting PartyDao Multi-sig address');
+    // TODO: setAddress or setUint256?
+    globals.setAddress(LibGlobals.GLOBAL_DAO_WALLET, PARTY_DAO_MULTISIG);
+    console.log('  Globals - successfully set PartyDao multi-sig address', PARTY_DAO_MULTISIG);
+
+    console.log('  Globals - setting PartyDao split basis points');
+    globals.setUint256(LibGlobals.GLOBAL_DAO_DISTRIBUTION_SPLIT, PARTY_DAO_DISTRIBUTION_SPLIT_BPS);
+    console.log('  Globals - successfully set PartyDao split basis points', PARTY_DAO_DISTRIBUTION_SPLIT_BPS);
+
 
     // DEPLOY_TOKEN_DISTRIBUTOR
+    console.log('');
     console.log('Deploying - TokenDistributor');
-    tokenDistributorAddress = new TokenDistributor(globals);
-    console.log('Deployed - TokenDistributor', address(tokenDistributorAddress));
+    tokenDistributor = new TokenDistributor(globals);
+    console.log('Deployed - TokenDistributor', address(tokenDistributor));
 
-    // DEPLOY_PARTY_CROWDFUND_FACTORY
-    // console.log('Deploying - PartyCrowdfunFactory');
-    // partyCrowdfundFactoryAddress = new PartyCrowdfunFactory();
-    // console.log('Deployed - PartyCrowdfunFactory', address(partyCrowdfundFactoryAddress));
+    console.log('  Globals - setting Token Distributor address');
+    globals.setAddress(LibGlobals.GLOBAL_TOKEN_DISTRIBUTOR, address(tokenDistributor));
+    console.log('  Globals - successfully set Token Distributor address', address(tokenDistributor));
+
+
+    // DEPLOY_SHARED_WYVERN_V2_MAKER
+    console.log('');
+    console.log('Deploying - SharedWyvernV2Maker');
+    openseaExchange = IWyvernExchangeV2(OPENSEA_EXCHANGE_ADDRESS);
+    sharedWyvernV2Maker = new SharedWyvernV2Maker(openseaExchange);
+    console.log('Deployed - SharedWyvernV2Maker', address(sharedWyvernV2Maker));
+
+    console.log('  Globals - setting OpenSea Zora auction duration');
+    globals.setUint256(LibGlobals.GLOBAL_OS_ZORA_AUCTION_DURATION, OS_ZORA_AUCTION_DURATION);
+    console.log('  Globals - successfully set OpenSea Zora auction duration', OS_ZORA_AUCTION_DURATION);
+
+
+    // DEPLOY_PROPOSAL_EXECUTION_ENGINE
+    console.log('');
+    console.log('Deploying - ProposalExecutionEngine');
+    zoraAuctionHouse = IZoraAuctionHouse(ZORA_AUCTION_HOUSE_ADDRESS);
+    proposalEngineImpl = new ProposalExecutionEngine(globals, sharedWyvernV2Maker, zoraAuctionHouse);
+    console.log('Deployed - ProposalExecutionEngine', address(sharedWyvernV2Maker));
+
+    console.log('  Globals - setting Proposal engine implementation address');
+    globals.setAddress(LibGlobals.GLOBAL_PROPOSAL_ENGINE_IMPL, address(proposalEngineImpl));
+    console.log('  Globals - successfully set Proposal engine implementation address', address(proposalEngineImpl));
+
+
+    // DEPLOY_PARTY_IMPLEMENTATION
+    console.log('');
+    console.log('Deploying - Party implementation');
+    partyImpl = new Party(globals);
+    console.log('Deployed - Party implementation', address(partyImpl));
+
+    console.log('  Globals - setting Party implementation address');
+    globals.setAddress(LibGlobals.GLOBAL_PARTY_IMPL, address(partyImpl));
+    console.log('  Globals - successfully set Party implementation address', address(partyImpl));
+
+
+    // DEPLOY_PARTY_FACTORY
+    console.log('');
+    console.log('Deploying - PartyFactory');
+    partyFactory = new PartyFactory(globals);
+    console.log('Deployed - PartyFactory', address(partyFactory));
+
+    console.log('  Globals - setting Party Factory address');
+    globals.setAddress(LibGlobals.GLOBAL_PARTY_FACTORY, address(partyFactory));
+    console.log('  Globals - successfully set Party Factory address', address(partyFactory));
+
+
+    // TODO: DEPLOY_PARTY_CROWDFUND_FACTORY
+    // console.log('');
+    // console.log('Deploying - PartyCrowdfundFactory');
+    // partyCrowdfundFactoryAddress = new PartyCrowdfundFactory();
+    // console.log('Deployed - PartyCrowdfundFactory', address(partyCrowdfundFactoryAddress));
 
     vm.stopBroadcast();
+    console.log('');
     console.log('Ending deploy script.');
   }
 }
