41469c4ef03c601d36cda3932b52226e2e58984c
Marcus Molchany <marcusmolchany@gmail.com>
2022-07-18 13:40:07 -0400

    update deploy constants

--- deploy/LibDeployConstants.sol
+++ deploy/LibDeployConstants.sol
@@ -5,7 +5,9 @@ pragma solidity ^0.8;
 
 library LibDeployConstants {
   uint256 internal constant PARTY_DAO_DISTRIBUTION_SPLIT_BPS = 250;
-  uint256 internal constant OS_ZORA_AUCTION_DURATION = 86400; // 60 * 60 * 24 = 86400 seconds = 24 hours
+  // todo: use for mainnet deploy
+  // uint256 internal constant OS_ZORA_AUCTION_DURATION = 86400; // 60 * 60 * 24 = 86400 seconds = 24 hours
+  uint256 internal constant OS_ZORA_AUCTION_DURATION = 2 minutes;
 
   struct DeployConstants {
     address[3] adminAddresses; // todo: change size of array based on deploy
@@ -44,13 +46,13 @@ library LibDeployConstants {
   function rinkeby() internal pure returns (DeployConstants memory) {
     DeployConstants memory rinkebyDeployConstants = DeployConstants({
       adminAddresses: [
-        0x0000000000000000000000000000000000000000,
-        0x000000000000000000000000000000000000dEaD,
-        0x0000000000000000000000000000000000001337
+        0x8fDC86689f5F35F2b4d9f649c7bdc9C64f59e6bD,
+        0x66512B61F855478bfba669e32719dE5fD7a57Fa4,
+        0x678e8bd1D8845399c8e3C1F946CB4309014456a5
       ],
       seaportExchangeAddress: 0x00000000006c3852cbEf3e08E8dF289169EdE581,
-      osZoraAuctionDuration: 6 minutes,
-      osZoraAuctionTimeout: 6 minutes,
+      osZoraAuctionDuration: 2 minutes,
+      osZoraAuctionTimeout: 2 minutes,
       partyDaoMultisig: 0xF7f52Dd34bc21eDA08c0b804C7c1dbc48375820f,
       partyDaoDistributionSplitBps: PARTY_DAO_DISTRIBUTION_SPLIT_BPS,
       zoraAuctionHouseAddress: 0xE7dd1252f50B3d845590Da0c5eADd985049a03ce,
