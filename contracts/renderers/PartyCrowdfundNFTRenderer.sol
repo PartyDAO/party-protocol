1359f146f2308124597a9fe90768ab2abc4d5204
Marcus Molchany <marcusmolchany@gmail.com>
2022-08-09 16:22:48 -0700

    add PartyCrowdfundNFTRenderer

---
+++ contracts/renderers/PartyCrowdfundNFTRenderer.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8;
+
+import "../utils/LibSafeCast.sol";
+import "../utils/Strings.sol";
+import "../utils/Base64.sol";
+
+import "./IERC721Renderer.sol";
+import "../globals/IGlobals.sol";
+import "../crowdfund/PartyCrowdfund.sol";
+
+contract PartyCrowdfundNFTRenderer is IERC721Renderer {
+    using LibSafeCast for uint256;
+
+    error InvalidTokenIdError();
+
+    IGlobals immutable _GLOBALS;
+
+    string constant baseStyle = 'base';
+
+    constructor(IGlobals globals) {
+        _GLOBALS = globals;
+    }
+
+    function textLine(string memory text, uint256 xPos, uint256 yPos) internal pure returns (string memory) {
+        string[3] memory parts;
+
+        parts[0] = string(abi.encodePacked(
+            '<text x="',
+            Strings.toString(xPos),
+            '" y="',
+            Strings.toString(yPos),
+            '" class="',
+            // TODO: Parameterize
+            baseStyle,
+            '">'
+        ));
+        parts[1] = text;
+        parts[2] = '</text>';
+
+        return string(abi.encodePacked(
+            parts[0],
+            parts[1],
+            parts[2]
+        ));
+    }
+
+    function renderTokenName(uint256 tokenId) internal view returns (string memory) {
+        return string(abi.encodePacked(
+            name,
+            " #",
+            Strings.toString(tokenId)
+        ));
+    }
+
+    function renderTokenId(uint256 tokenId) internal pure returns (string memory) {
+        return string(abi.encodePacked('#', Strings.toString(tokenId)));
+    }
+
+    function renderOwnerAddress(uint256 tokenId) internal view returns (string memory) {
+        address owner = PartyCrowdfund(this)._ownerOf[tokenId];
+
+        return string(abi.encodePacked('Owner: ', Strings.toHexString(owner)));
+    }
+
+    function renderCrowdfundState() internal view returns (string memory crowdfundState) {
+      PartyCrowdfund.CrowdfundLifecycle cfl = PartyCrowdfund(this).getCrowdfundLifecycle();
+      if (cfl == PartyCrowdfund.CrowdfundLifecycle.Invalid) {
+          crowdfundState = "Invalid";
+      } else if (cfl == PartyCrowdfund.CrowdfundLifecycle.Active) {
+          crowdfundState = "Active";
+      } else if (cfl == PartyCrowdfund.CrowdfundLifecycle.Expired) {
+          crowdfundState = "Expired";
+      } else if (cfl == PartyCrowdfund.CrowdfundLifecycle.Busy) {
+          crowdfundState = "Busy";
+      } else if (cfl == PartyCrowdfund.CrowdfundLifecycle.Lost) {
+          crowdfundState = "Lost";
+      } else if (cfl == PartyCrowdfund.CrowdfundLifecycle.Won) {
+          crowdfundState = "Won";
+      } else {
+          crowdfundState = "Unknown";
+      }
+    }
+
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        if(PartyCrowdfund(this)._ownerOf[tokenId] == address(0)) {
+            revert InvalidTokenIdError();
+        }
+
+        string[7] memory svgParts;
+
+        svgParts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>text { fill: white; font-family: -apple-system, BlinkMacSystemFont, sans-serif; } .base { font-size: 11px; } .detail {font-size: 10px;}</style><rect width="100%" height="100%" fill="black" />';
+
+        svgParts[1] = textLine(PartyCrowdfund(this).name, 10, 20);
+        svgParts[3] = textLine(renderTokenId(tokenId), 300, 20);
+
+        svgParts[2] = textLine(PartyCrowdfund(this).symbol, 10, 60);
+
+        svgParts[4] = textLine(renderOwnerAddress(tokenId), 10, 120);
+
+        svgParts[5] = textLine(renderCrowdfundState(), 10, 80);
+
+        svgParts[6] = '</svg>';
+
+        string memory output = string(
+            abi.encodePacked(
+                svgParts[0], svgParts[1], svgParts[2],
+                svgParts[3], svgParts[4], svgParts[5],
+                svgParts[6]
+            )
+        );
+
+        string memory json = Base64.encode(bytes(
+            string(
+                abi.encodePacked(
+                    '{"name":"',
+                    renderTokenName(tokenId),
+                    '", "description": "PartyBid Crowdfund NFT", "image": "data:image/svg+xml;base64,',
+                    Base64.encode(bytes(output)),
+                    '"}'
+                )
+            )
+        ));
+
+        output = string(abi.encodePacked('data:application/json;base64,', json));
+
+        return output;
+    }
+}
