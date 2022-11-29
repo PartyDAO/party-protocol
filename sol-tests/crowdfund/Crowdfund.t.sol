f3e5d870eb2ed45f95cd461ab9b97281b658bd2b
Brian Le <brian@brianle.xyz>
2022-11-29 10:33:41 -0800

    test(crowdfund): add test for gatekeeper check

--- sol-tests/crowdfund/Crowdfund.t.sol
+++ sol-tests/crowdfund/Crowdfund.t.sol
@@ -59,8 +59,8 @@ contract CrowdfundTest is Test, TestUtils {
     address payable defaultSplitRecipient = payable(0);
     uint16 defaultSplitBps = 0.1e4;
     address defaultInitialDelegate;
-    IGateKeeper defaultGateKeeper;
-    bytes12 defaultGateKeeperId;
+    IGateKeeper gateKeeper;
+    bytes12 gateKeeperId;
     Crowdfund.FixedGovernanceOpts defaultGovernanceOpts;
     address dao;
     EmergencyExecuteTarget emergencyExecuteTarget = new EmergencyExecuteTarget();
@@ -107,10 +107,10 @@ contract CrowdfundTest is Test, TestUtils {
         }
     }
 
-    function _createTokens(
-        address owner,
-        uint256 count
-    ) private returns (IERC721[] memory tokens, uint256[] memory tokenIds) {
+    function _createTokens(address owner, uint256 count)
+        private
+        returns (IERC721[] memory tokens, uint256[] memory tokenIds)
+    {
         tokens = new IERC721[](count);
         tokenIds = new uint256[](count);
         for (uint256 i; i < count; ++i) {
@@ -141,8 +141,8 @@ contract CrowdfundTest is Test, TestUtils {
                                 splitBps: defaultSplitBps,
                                 initialContributor: initialContributor,
                                 initialDelegate: initialDelegate,
-                                gateKeeper: defaultGateKeeper,
-                                gateKeeperId: defaultGateKeeperId,
+                                gateKeeper: gateKeeper,
+                                gateKeeperId: gateKeeperId,
                                 governanceOpts: defaultGovernanceOpts
                             })
                         )
@@ -152,10 +152,10 @@ contract CrowdfundTest is Test, TestUtils {
         );
     }
 
-    function _createCrowdfund(
-        uint256 initialContribution,
-        uint256 customizationPresetId
-    ) private returns (TestableCrowdfund cf) {
+    function _createCrowdfund(uint256 initialContribution, uint256 customizationPresetId)
+        private
+        returns (TestableCrowdfund cf)
+    {
         return
             _createCrowdfund(
                 initialContribution,
@@ -169,10 +169,11 @@ contract CrowdfundTest is Test, TestUtils {
         return _createCrowdfund(initialContribution, address(this), defaultInitialDelegate, 0);
     }
 
-    function _createExpectedPartyOptions(
-        TestableCrowdfund cf,
-        uint256 finalPrice
-    ) private view returns (Party.PartyOptions memory opts) {
+    function _createExpectedPartyOptions(TestableCrowdfund cf, uint256 finalPrice)
+        private
+        view
+        returns (Party.PartyOptions memory opts)
+    {
         Crowdfund.FixedGovernanceOpts memory govOpts = cf.getFixedGovernanceOpts();
         return
             Party.PartyOptions({
@@ -195,10 +196,11 @@ contract CrowdfundTest is Test, TestUtils {
         return (uint256(1e4 - defaultSplitBps) * contribution) / 1e4;
     }
 
-    function _getAmountWithSplit(
-        uint256 contribution,
-        uint256 totalContributions
-    ) private view returns (uint256 r) {
+    function _getAmountWithSplit(uint256 contribution, uint256 totalContributions)
+        private
+        view
+        returns (uint256 r)
+    {
         return
             _getAmountWithoutSplit(contribution) +
             (uint256(defaultSplitBps) * totalContributions + (1e4 - 1)) /
@@ -772,8 +774,8 @@ contract CrowdfundTest is Test, TestUtils {
 
         AllowListGateKeeper gk = new AllowListGateKeeper();
         bytes12 gateId = gk.createGate(keccak256(abi.encodePacked(contributor1)));
-        defaultGateKeeper = gk;
-        defaultGateKeeperId = gateId;
+        gateKeeper = gk;
+        gateKeeperId = gateId;
         TestableCrowdfund cf = _createCrowdfund(0);
 
         // contributor1 contributes 1 ETH
@@ -788,7 +790,7 @@ contract CrowdfundTest is Test, TestUtils {
             abi.encodeWithSelector(
                 Crowdfund.NotAllowedByGateKeeperError.selector,
                 contributor2,
-                defaultGateKeeper,
+                gateKeeper,
                 gateId,
                 abi.encode(new bytes32[](0))
             )
@@ -925,8 +927,8 @@ contract CrowdfundTest is Test, TestUtils {
                                 splitBps: defaultSplitBps,
                                 initialContributor: address(0),
                                 initialDelegate: address(this),
-                                gateKeeper: defaultGateKeeper,
-                                gateKeeperId: defaultGateKeeperId,
+                                gateKeeper: gateKeeper,
+                                gateKeeperId: gateKeeperId,
                                 governanceOpts: defaultGovernanceOpts
                             })
                         )
@@ -970,6 +972,30 @@ contract CrowdfundTest is Test, TestUtils {
         vm.prank(contributor);
         cf.contributeFor{ value: 1e18 }(recipient, "");
         assertEq(cf.delegationsByContributor(recipient), delegate);
+    }
+
+    function test_contributeFor_gatekeeperAllowsSenderToContributeForOthers() external {
+        address contributor = _randomAddress();
+        address recipient = _randomAddress();
+        AllowListGateKeeper gk = new AllowListGateKeeper();
+        bytes12 gateId = gk.createGate(keccak256(abi.encodePacked(contributor)));
+        gateKeeper = gk;
+        gateKeeperId = gateId;
+        TestableCrowdfund cf = _createCrowdfund(0);
+        // Contributor contributes on recipient's behalf (with gatekeeper)
+        vm.deal(contributor, 1e18);
+        vm.prank(contributor);
+        cf.contributeFor{ value: 1e18 }(recipient, abi.encode(new bytes32[](0)));
+        assertEq(cf.getContributionEntriesByContributorCount(contributor), 0);
+        assertEq(cf.getContributionEntriesByContributorCount(recipient), 1);
+        (uint256 ethContributed, uint256 ethUsed, uint256 ethOwed, uint256 votingPower) = cf
+            .getContributorInfo(recipient);
+        assertEq(ethContributed, 1e18);
+        assertEq(ethUsed, 0);
+        assertEq(ethOwed, 0);
+        assertEq(votingPower, 0);
+        assertEq(uint256(cf.totalContributions()), 1e18);
+        assertEq(cf.delegationsByContributor(recipient), recipient);
     }
 
     function test_canReuseContributionEntry() external {
