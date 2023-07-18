29a5317ac8e9594643d2689e1b96d2dd7506e04e
Arr00 <13561405+arr00@users.noreply.github.com>
2023-07-18 16:50:12 -0400

    chore: revoke zora approval on cancel (#254)

--- contracts/proposals/ListOnZoraProposal.sol
+++ contracts/proposals/ListOnZoraProposal.sol
@@ -179,6 +179,7 @@ contract ListOnZoraProposal is ZoraHelpers {
             }
             // minExpiry passed with no bids
             ZORA.cancelAuction(token, tokenId);
+            IERC721(token).approve(address(0), tokenId);
             emit ZoraAuctionExpired(token, tokenId, minExpiry);
             return ZoraAuctionStatus.Expired;
         } else {
