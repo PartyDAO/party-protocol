ce3b47dc320158c09a875df6372c4d87e9c58320
Arr00 <13561405+arr00@users.noreply.github.com>
2023-07-31 16:00:00 -0400

    test: Fix zora fork tests

---
+++ test/utils/SetupPartyHelper.sol
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8;
+
+import { Vm } from "forge-std/Test.sol";
+import { TestUtils } from "../TestUtils.sol";
+import { IERC721 } from "../../contracts/tokens/IERC721.sol";
+import { GlobalsAdmin } from "../TestUsers.sol";
+import { PartyFactory } from "../../contracts/party/PartyFactory.sol";
+import { Globals } from "../../contracts/globals/Globals.sol";
+import { Party } from "../../contracts/party/Party.sol";
+import { ProposalExecutionEngine } from "../../contracts/proposals/ProposalExecutionEngine.sol";
+import { IFractionalV1VaultFactory } from "../../contracts/proposals/vendor/FractionalV1.sol";
+import { MockZoraReserveAuctionCoreEth } from "../proposals/MockZoraReserveAuctionCoreEth.sol";
+import { IReserveAuctionCoreEth } from "../../contracts/vendor/markets/IReserveAuctionCoreEth.sol";
+import { PartyGovernance } from "../../contracts/party/PartyGovernance.sol";
+import { ERC721Receiver } from "../../contracts/tokens/ERC721Receiver.sol";
+
+abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
+    bool private immutable _isForked;
+    GlobalsAdmin globalsAdmin;
+    Party party;
+    Globals private globals;
+    PartyFactory private partyFactory;
+    uint256 internal johnPk = 0xa11ce;
+    uint256 internal dannyPk = 0xb0b;
+    uint256 internal stevePk = 0xca1;
+    address internal john = vm.addr(johnPk);
+    address internal danny = vm.addr(dannyPk);
+    address internal steve = vm.addr(stevePk);
+    IERC721[] private preciousTokens = new IERC721[](0);
+    uint256[] private preciousTokenIds = new uint256[](0);
+    uint40 private constant _EXECUTION_DELAY = 300;
+
+    constructor(bool isForked) {
+        _isForked = isForked;
+    }
+
+    function setUp() public virtual {
+        globalsAdmin = new GlobalsAdmin();
+        globals = globalsAdmin.globals();
+        Party partyImpl = new Party(globals);
+        address globalDaoWalletAddress = address(420);
+        globalsAdmin.setGlobalDaoWallet(globalDaoWalletAddress);
+
+        ProposalExecutionEngine pe = new ProposalExecutionEngine(
+            globals,
+            _isForked
+                ? IReserveAuctionCoreEth(0x5f7072E1fA7c01dfAc7Cf54289621AFAaD2184d0)
+                : new MockZoraReserveAuctionCoreEth(),
+            _isForked
+                ? IFractionalV1VaultFactory(0x85Aa7f78BdB2DE8F3e0c0010d99AD5853fFcfC63)
+                : IFractionalV1VaultFactory(address(0))
+        );
+
+        globalsAdmin.setProposalEng(address(pe));
+
+        Party.PartyOptions memory opts;
+        address[] memory hosts = new address[](1);
+        hosts[0] = address(420);
+        opts.name = "PARTY";
+        opts.symbol = "PR-T";
+        opts.governance.hosts = hosts;
+        opts.governance.voteDuration = 99;
+        opts.governance.executionDelay = _EXECUTION_DELAY;
+        opts.governance.passThresholdBps = 1000;
+        opts.governance.totalVotingPower = 301;
+
+        partyFactory = new PartyFactory();
+        address[] memory authorities = new address[](1);
+        authorities[0] = address(this);
+        party = partyFactory.createParty(
+            partyImpl,
+            authorities,
+            opts,
+            preciousTokens,
+            preciousTokenIds,
+            0
+        );
+        party.mint(john, 100, john);
+        party.mint(danny, 100, danny);
+        party.mint(steve, 100, steve);
+        party.mint(address(this), 1, address(this));
+        vm.warp(block.timestamp + 100);
+        vm.roll(block.number + 10);
+    }
+
+    function proposeAndPassProposal(
+        PartyGovernance.Proposal memory proposal
+    ) internal returns (uint256 proposalId) {
+        vm.prank(john);
+        proposalId = party.propose(proposal, 0);
+
+        vm.warp(block.timestamp + _EXECUTION_DELAY);
+    }
+
+    function proposePassAndExecuteProposal(
+        PartyGovernance.Proposal memory proposal
+    ) internal returns (uint256, bytes memory) {
+        uint256 proposalId = proposeAndPassProposal(proposal);
+        bytes memory progressData = executeProposal(proposalId, proposal);
+        return (proposalId, progressData);
+    }
+
+    function executeProposal(
+        uint256 proposalId,
+        PartyGovernance.Proposal memory proposal
+    ) internal returns (bytes memory) {
+        return executeProposal(proposalId, proposal, "");
+    }
+
+    struct Log {
+        bytes32[] topics;
+        bytes data;
+        address emitter;
+    }
+
+    function executeProposal(
+        uint256 proposalId,
+        PartyGovernance.Proposal memory proposal,
+        bytes memory progressData
+    ) internal returns (bytes memory) {
+        vm.recordLogs();
+        party.execute(proposalId, proposal, preciousTokens, preciousTokenIds, progressData, "");
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        bytes memory nextProgressData;
+
+        for (uint i = 0; i < entries.length; i++) {
+            if (entries[i].emitter != address(party)) {
+                continue;
+            }
+            if (entries[i].topics[0] != keccak256("ProposalExecuted(uint256,address,bytes)")) {
+                continue;
+            }
+
+            bytes memory entryData = entries[i].data;
+            (, nextProgressData) = abi.decode(entryData, (address, bytes));
+        }
+
+        return nextProgressData;
+    }
+}
