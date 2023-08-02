8f3ab68da0f0404bc829e798e47e4b1cf5d51e92
Arr00 <13561405+arr00@users.noreply.github.com>
2023-08-02 16:13:47 -0400

    feat: ERC1271 signatures (#263)
    * initial work signature validation
    
    * test functionality
    
    * check `tx.origin` instead of `msg.sender`
    
    * test: run signature validation override off-chain
    
    * test: add off-chain validator tests
    
    * test: add voting power tests
    
    * test: delegated less than on BIP
    
    * minor touchups
    
    * reduce calldata size off-chain validation
    
    * remove unnecessary encode call
    
    * Bips ➡️ Bps
    
    * test: simplify tests
    
    * fix capitalization
    
    * fix spelling error and add comments

--- test/utils/SetupPartyHelper.sol
+++ test/utils/SetupPartyHelper.sol
@@ -16,10 +16,18 @@ import { PartyGovernance } from "../../contracts/party/PartyGovernance.sol";
 import { ERC721Receiver } from "../../contracts/tokens/ERC721Receiver.sol";
 import { MetadataRegistry } from "../../contracts/renderers/MetadataRegistry.sol";
 import { TokenDistributor } from "../../contracts/distribution/TokenDistributor.sol";
+import { OffChainSignatureValidator } from "../../contracts/signature-validators/OffChainSignatureValidator.sol";
 
 /// @notice This contract provides a fully functioning party instance for testing.
 ///     Run setup from inheriting contract.
 abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
+    struct SetupPartyParams {
+        uint96 johnVotes;
+        uint96 dannyVotes;
+        uint96 steveVotes;
+        uint96 thisVotes;
+    }
+
     bool private immutable _isForked;
     GlobalsAdmin internal globalsAdmin;
     Party internal party;
@@ -43,6 +51,14 @@ abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
 
     constructor(bool isForked) {
         _isForked = isForked;
+    }
+
+    function setUpWithParams(SetupPartyParams memory params) public {
+        johnVotes = params.johnVotes;
+        dannyVotes = params.dannyVotes;
+        steveVotes = params.steveVotes;
+        thisVotes = params.thisVotes;
+        SetupPartyHelper.setUp();
     }
 
     function setUp() public virtual {
@@ -76,6 +92,9 @@ abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
         MetadataRegistry metadataRegistry = new MetadataRegistry(globals, registrars);
         globalsAdmin.setMetadataRegistry(address(metadataRegistry));
 
+        OffChainSignatureValidator offChainGlobalValidator = new OffChainSignatureValidator();
+        globalsAdmin.setOffChainSignatureValidator(address(offChainGlobalValidator));
+
         johnVotes = johnVotes == 0 ? 100 : johnVotes;
         dannyVotes = dannyVotes == 0 ? 100 : dannyVotes;
         steveVotes = steveVotes == 0 ? 100 : steveVotes;
@@ -90,9 +109,13 @@ abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
         opts.governance.voteDuration = 99;
         opts.governance.executionDelay = _EXECUTION_DELAY;
         opts.governance.passThresholdBps = 1000;
+<<<<<<< HEAD
         opts.governance.totalVotingPower = 301;
         opts.proposalEngine.allowArbCallsToSpendPartyEth = true;
         opts.proposalEngine.distributionsRequireVote = true;
+=======
+        opts.governance.totalVotingPower = johnVotes + dannyVotes + steveVotes + thisVotes;
+>>>>>>> 7de17c1 (feat: ERC1271 signatures (#263))
 
         address[] memory authorities = new address[](1);
         authorities[0] = address(this);
@@ -104,10 +127,10 @@ abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
             preciousTokenIds,
             0
         );
-        party.mint(john, 100, john);
-        party.mint(danny, 100, danny);
-        party.mint(steve, 100, steve);
-        party.mint(address(this), 1, address(this));
+        party.mint(john, johnVotes, john);
+        party.mint(danny, dannyVotes, danny);
+        party.mint(steve, steveVotes, steve);
+        party.mint(address(this), thisVotes, address(this));
         vm.warp(block.timestamp + 100);
         vm.roll(block.number + 10);
     }
@@ -115,7 +138,7 @@ abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
     /// @notice Propose pass and wait for the execution delay of a proposal
     /// @param proposal The `PartyGovernance.Proposal` struct representing the proposal
     /// @return proposalId The proposal id for the proposal
-    function proposeAndPassProposal(
+    function _proposeAndPassProposal(
         PartyGovernance.Proposal memory proposal
     ) internal returns (uint256 proposalId) {
         vm.prank(john);
@@ -128,11 +151,11 @@ abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
     /// @param proposal The `PartyGovernance.Proposal` struct representing the proposal
     /// @return proposalId The proposal id for the proposal
     /// @return progressData The progress data returned from the proposal execution
-    function proposePassAndExecuteProposal(
+    function _proposePassAndExecuteProposal(
         PartyGovernance.Proposal memory proposal
     ) internal returns (uint256, bytes memory) {
-        uint256 proposalId = proposeAndPassProposal(proposal);
-        bytes memory progressData = executeProposal(proposalId, proposal);
+        uint256 proposalId = _proposeAndPassProposal(proposal);
+        bytes memory progressData = _executeProposal(proposalId, proposal);
         return (proposalId, progressData);
     }
 
@@ -140,11 +163,11 @@ abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
     /// @param proposalId The proposal id for the proposal
     /// @param proposal The `PartyGovernance.Proposal` struct representing the proposal
     /// @return progressData The progress data returned from the proposal execution
-    function executeProposal(
+    function _executeProposal(
         uint256 proposalId,
         PartyGovernance.Proposal memory proposal
     ) internal returns (bytes memory) {
-        return executeProposal(proposalId, proposal, "");
+        return _executeProposal(proposalId, proposal, "");
     }
 
     /// @notice Execute the given proposal with `progressData`
@@ -152,7 +175,7 @@ abstract contract SetupPartyHelper is TestUtils, ERC721Receiver {
     /// @param proposal The `PartyGovernance.Proposal` struct representing the proposal
     /// @param progressData The progress data to pass to the proposal execution
     /// @return progressData The progress data returned from the proposal execution
-    function executeProposal(
+    function _executeProposal(
         uint256 proposalId,
         PartyGovernance.Proposal memory proposal,
         bytes memory progressData
