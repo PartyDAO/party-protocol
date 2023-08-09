c1e3479c274f9a052115d740c9ecc7b1a9fc14fa
Arr00 <13561405+arr00@users.noreply.github.com>
2023-08-09 14:23:00 -0800

    test: create with metadata

--- test/crowdfund/AtomicManualParty.t.sol
+++ test/crowdfund/AtomicManualParty.t.sol
@@ -7,6 +7,8 @@ import { AtomicManualParty } from "../../contracts/crowdfund/AtomicManualParty.s
 import { Party } from "../../contracts/party/Party.sol";
 import { Proxy } from "../../contracts/utils/Proxy.sol";
 import { IERC721 } from "../../contracts/tokens/IERC721.sol";
+import { MetadataProvider } from "../../contracts/renderers/MetadataProvider.sol";
+import { IMetadataProvider } from "../../contracts/renderers/IMetadataProvider.sol";
 
 contract AtomicManualPartyTest is SetupPartyHelper {
     event PartyCreated(
@@ -16,6 +18,7 @@ contract AtomicManualPartyTest is SetupPartyHelper {
         uint256[] preciousTokenIds,
         address creator
     );
+    event ProviderSet(address indexed instance, IMetadataProvider indexed provider);
 
     AtomicManualParty private atomicManualParty;
 
@@ -59,6 +62,51 @@ contract AtomicManualPartyTest is SetupPartyHelper {
             preciousTokens,
             preciousTokenIds,
             0,
+            partyMembers,
+            partyMemberVotes
+        );
+
+        // Ensure `atomicManualParty` is not an authority after creation
+        assertFalse(party.isAuthority(address(atomicManualParty)));
+    }
+
+    function test_createAtomicManualPartyWithMetadata() public {
+        Party.PartyOptions memory opts;
+        opts.name = "PARTY";
+        opts.symbol = "PR-T";
+        opts.governance.voteDuration = 99;
+        opts.governance.executionDelay = _EXECUTION_DELAY;
+        opts.governance.passThresholdBps = 1000;
+        opts.governance.totalVotingPower = 180;
+
+        address[] memory partyMembers = new address[](2);
+        uint96[] memory partyMemberVotes = new uint96[](2);
+
+        partyMembers[0] = john;
+        partyMembers[1] = danny;
+
+        partyMemberVotes[0] = 100;
+        partyMemberVotes[1] = 80;
+
+        // Not checking address of the party
+        vm.expectEmit(false, true, true, true);
+        emit PartyCreated(
+            Party(payable(0)),
+            opts,
+            preciousTokens,
+            preciousTokenIds,
+            address(atomicManualParty)
+        );
+        vm.expectEmit(false, true, true, true);
+        emit ProviderSet(address(0), IMetadataProvider(address(0)));
+        atomicManualParty.createPartyWithMetadata(
+            Party(payable(address(Proxy(payable(address(party))).IMPL()))),
+            opts,
+            preciousTokens,
+            preciousTokenIds,
+            0,
+            MetadataProvider(address(0)),
+            "",
             partyMembers,
             partyMemberVotes
         );
