fd57bdfee7d8c0282fa65736fbdc633a04c12cfd
Arr00 <13561405+arr00@users.noreply.github.com>
2023-08-23 14:08:44 -0400

    update addresses

--- deploy/LibDeployConstants.sol
+++ deploy/LibDeployConstants.sol
@@ -85,10 +85,10 @@ library LibDeployConstants {
             osZone: 0x0000000000000000000000000000000000000000,
             osConduitKey: 0xf984c55ca75735630c1c27d3d06969c1aa6af1df86d22ddc0e3a978ad6138e9f,
             osConduitController: 0x00000000F9490004C11Cef243f5400493c00Ad63,
-            fractionalVaultFactory: 0x014850E83d9D0D1BB0c8624035F09626b967B81c,
-            foundationMarket: 0xeB1bD095061bbDb1aD065524628812cae63e4222,
-            nounsAuctionHouse: 0x7295e70f2B26986Ba108bD1Bf9E349a181F4a6Ea,
-            zoraReserveAuctionCoreEth: 0x2506D9F5A2b0E1A2619bCCe01CD3e7C289A13163,
+            fractionalVaultFactory: 0x0000000000000000000000000000000000000000,
+            foundationMarket: 0x0000000000000000000000000000000000000000,
+            nounsAuctionHouse: 0x0000000000000000000000000000000000000000,
+            zoraReserveAuctionCoreEth: 0x0000000000000000000000000000000000000000,
             networkName: "base-goerli",
             deployedNounsMarketWrapper: 0x0000000000000000000000000000000000000000,
             deployedFoundationMarketWrapper: 0x0000000000000000000000000000000000000000
@@ -147,18 +147,18 @@ library LibDeployConstants {
             minCancelDelay: 6 weeks,
             maxCancelDelay: 12 weeks,
             distributorEmergencyActionAllowedDuration: 365 days,
-            partyDaoMultisig: 0xF7f52Dd34bc21eDA08c0b804C7c1dbc48375820f,
+            partyDaoMultisig: 0xF498fd75Ee8D35294952343f1A77CAE5EA5aF6AA,
             allowedERC20SwapOperatorTargets: allowedERC20SwapOperatorTargets,
             osZone: 0x0000000000000000000000000000000000000000,
             osConduitKey: 0xf984c55ca75735630c1c27d3d06969c1aa6af1df86d22ddc0e3a978ad6138e9f,
             osConduitController: 0x00000000F9490004C11Cef243f5400493c00Ad63,
-            fractionalVaultFactory: 0x85Aa7f78BdB2DE8F3e0c0010d99AD5853fFcfC63,
-            foundationMarket: 0xcDA72070E455bb31C7690a170224Ce43623d0B6f,
-            nounsAuctionHouse: 0x830BD73E4184ceF73443C15111a1DF14e495C706,
-            zoraReserveAuctionCoreEth: 0x5f7072E1fA7c01dfAc7Cf54289621AFAaD2184d0,
+            fractionalVaultFactory: 0x0000000000000000000000000000000000000000,
+            foundationMarket: 0x0000000000000000000000000000000000000000,
+            nounsAuctionHouse: 0x0000000000000000000000000000000000000000,
+            zoraReserveAuctionCoreEth: 0x0000000000000000000000000000000000000000,
             networkName: "base",
-            deployedNounsMarketWrapper: 0x9319DAd8736D752C5c72DB229f8e1b280DC80ab1,
-            deployedFoundationMarketWrapper: 0x96e5b0519983f2f984324b926e6d28C3A4Eb92A1
+            deployedNounsMarketWrapper: 0x0000000000000000000000000000000000000000,
+            deployedFoundationMarketWrapper: 0x0000000000000000000000000000000000000000
         });
 
         return deployConstants;
