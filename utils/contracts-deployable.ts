31eee6e59b88c1303ed2aa502291fe24802da5b2
Arr00 <13561405+arr00@users.noreply.github.com>
2023-10-04 14:00:06 -0700

    chore: further reduce contract size

--- utils/contracts-deployable.ts
+++ utils/contracts-deployable.ts
@@ -1,6 +1,4 @@
 import childProcess from "child_process";
-import fs from "fs";
-import { glob } from "glob";
 
 async function run(command: string) {
   const result = childProcess.spawnSync(command, [], {
@@ -20,32 +18,7 @@ async function main() {
   await run("rm -rf test/");
   await run("rm -rf deploy/");
   const buildArgs = process.argv.slice(2).join(" ");
-  await run(`forge build ${buildArgs}`);
-  const files = await glob("out/**/*.json");
-  for (const file of files) {
-    checkContractSize(file);
-  }
-}
-
-function checkContractSize(contractFile: string) {
-  const fileContents = JSON.parse(fs.readFileSync(contractFile, "utf8"));
-  if (
-    !fileContents.deployedBytecode.hasOwnProperty("object") ||
-    fileContents.deployedBytecode.object == "0x"
-  ) {
-    return true;
-  }
-  const deployedBytecodeSize = (fileContents.deployedBytecode.object.length - 2) / 2;
-  if (deployedBytecodeSize > 24576) {
-    throw new Error(
-      `Contract ${contractFile} is too large to deploy. ${deployedBytecodeSize} bytes`,
-    );
-  }
-  console.log(
-    `Contract ${
-      contractFile.split("/").at(-1).split(".")[0]
-    } is deployable. Size: ${deployedBytecodeSize} bytes`,
-  );
+  await run(`forge build ${buildArgs} --sizes`);
 }
 
 main();
