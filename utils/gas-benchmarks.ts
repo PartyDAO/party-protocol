84fc893c4d9d1f3753d1527f8b89b7cac9b47bd7
Arr00 <13561405+arr00@users.noreply.github.com>
2023-10-11 16:08:20 -0300

    add script to run benchmarks

---
+++ utils/gas-benchmarks.ts
@@ -0,0 +1,40 @@
+import childProcess from "child_process";
+import fs from "fs";
+
+async function run(command: string) {
+  const result = childProcess.spawnSync(command, [], {
+    shell: true,
+    stdio: "inherit",
+  });
+
+  if (result.status !== 0) {
+    throw new Error(`Command "${command}" failed with status code ${result.status}`);
+  }
+
+  return result.stdout;
+}
+
+async function main() {
+  childProcess.exec("forge test --match-contract GasBenchmarks -vv", (error, stdout, stderr) => {
+    const outputLines = stdout.split("\n");
+    let wantedOutput = new Array<string>();
+    for (let i = 0; i < outputLines.length; i++) {
+      const outputLine = outputLines[i].trim();
+      if (
+        outputLine.startsWith("Logs") ||
+        outputLine.includes("[PASS]") ||
+        outputLine == "" ||
+        outputLine.startsWith("Test result:") ||
+        outputLine.startsWith("Ran 1 test") ||
+        outputLine.endsWith("test/GasBenchmarks.t.sol:GasBenchmarks") ||
+        outputLine.startsWith("No files changed")
+      ) {
+        continue;
+      }
+      wantedOutput.push(outputLine);
+    }
+    fs.writeFileSync("gas-results.txt", wantedOutput.join("\n"));
+  });
+}
+
+main();
