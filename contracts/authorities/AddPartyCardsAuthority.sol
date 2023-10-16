a3fc74f7185c9e592366701a1702e9253aeb3273
Arr00 <13561405+arr00@users.noreply.github.com>
2023-10-16 15:53:48 -0300

    skeleton

---
+++ contracts/authorities/AddPartyCardsAuthority.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+contract AddPartyCardsAuthority {
+    function addPartyCards(
+        address[] calldata newPartyMembers,
+        uint96[] calldata newPartyMemberVotingPowers
+    ) external {}
+}
