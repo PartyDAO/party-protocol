7ce9831cdfbebd83dd20b5ee612d0f786b920b60
Arr00 <13561405+arr00@users.noreply.github.com>
2023-11-21 18:58:35 -0500

    cache governance settings

--- contracts/party/PartyGovernance.sol
+++ contracts/party/PartyGovernance.sol
@@ -129,6 +129,9 @@ abstract contract PartyGovernance is
         uint8 numHosts;
         /// @notice Number of hosts that accepted proposal
         uint8 numHostsAccepted;
+        uint40 voteDuration;
+        uint40 executionDelay;
+        uint16 passThresholdBps;
     }
 
     // Storage states for a proposal.
@@ -541,6 +544,9 @@ abstract contract PartyGovernance is
     ) external returns (uint256 proposalId) {
         _assertActiveMember();
         proposalId = ++lastProposalId;
+
+        ProposalStorage.GovernanceValues memory gv = _getSharedProposalStorage().governanceValues;
+
         // Store the time the proposal was created and the proposal hash.
         (
             _proposalStateByProposalId[proposalId].values,
@@ -552,9 +558,12 @@ abstract contract PartyGovernance is
                 executedTime: 0,
                 completedTime: 0,
                 votes: 0,
-                totalVotingPower: _getSharedProposalStorage().governanceValues.totalVotingPower,
+                totalVotingPower: gv.totalVotingPower,
                 numHosts: numHosts,
-                numHostsAccepted: 0
+                numHostsAccepted: 0,
+                voteDuration: gv.voteDuration,
+                executionDelay: gv.executionDelay,
+                passThresholdBps: gv.passThresholdBps
             }),
             getProposalHash(proposal)
         );
@@ -625,7 +634,7 @@ abstract contract PartyGovernance is
         if (
             values.passedTime == 0 &&
             (uint256(values.votes) * 1e4) / uint256(values.totalVotingPower) >=
-            uint256(_getSharedProposalStorage().governanceValues.passThresholdBps)
+            uint256(values.passThresholdBps)
         ) {
             info.values.passedTime = uint40(block.timestamp);
             emit ProposalPassed(proposalId);
@@ -1055,10 +1064,9 @@ abstract contract PartyGovernance is
             return ProposalStatus.Defeated;
         }
         uint40 t = uint40(block.timestamp);
-        GovernanceValues memory gv = _getSharedProposalStorage().governanceValues;
         if (pv.passedTime != 0) {
             // Ready.
-            if (pv.passedTime + gv.executionDelay <= t) {
+            if (pv.passedTime + pv.executionDelay <= t) {
                 return ProposalStatus.Ready;
             }
             // If unanimous, we skip the execution delay.
@@ -1073,7 +1081,7 @@ abstract contract PartyGovernance is
             return ProposalStatus.Passed;
         }
         // Voting window expired.
-        if (pv.proposedTime + gv.voteDuration <= t) {
+        if (pv.proposedTime + pv.voteDuration <= t) {
             return ProposalStatus.Defeated;
         }
         return ProposalStatus.Voting;
