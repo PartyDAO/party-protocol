60391db0794cf6a72a5730eb25bb1ffae5a87e5c
Brian Le <brian@brianle.xyz>
2024-02-15 13:17:49 -0500

    fix distributor

--- contracts/distribution/PushDistributor.sol
+++ contracts/distribution/PushDistributor.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
+import { PartyGovernance } from "./../party/PartyGovernance.sol";
 import { IERC20 } from "../tokens/IERC20.sol";
 import { LibSafeCast } from "../utils/LibSafeCast.sol";
 import { LibERC20Compat } from "./../utils/LibERC20Compat.sol";
@@ -24,6 +25,15 @@ interface IParty {
         uint256 timestamp,
         uint256 hintIndex
     ) external view returns (uint96);
+    function getProposalStateInfo(
+        uint256 proposalId
+    )
+        external
+        view
+        returns (
+            PartyGovernance.ProposalStatus status,
+            PartyGovernance.ProposalStateValues memory values
+        );
 }
 
 contract PushDistributor {
@@ -34,7 +44,12 @@ contract PushDistributor {
     // Token address used to indicate ETH.
     address private constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
-    function distribute(IERC20 token, address[] memory members, uint256 amount) external payable {
+    function distribute(
+        IERC20 token,
+        address[] memory members,
+        uint256 amount,
+        uint256 proposalId
+    ) external payable {
         IParty party = IParty(payable(msg.sender));
         if (address(token) == ETH_ADDRESS) {
             require(msg.value == amount, "Incorrect ETH amount");
@@ -43,9 +58,26 @@ contract PushDistributor {
             token.compatTransferFrom(msg.sender, address(this), amount);
         }
 
+        uint40 proposedTime;
+        uint96 totalVotingPower;
+        {
+            (
+                PartyGovernance.ProposalStatus status,
+                PartyGovernance.ProposalStateValues memory proposal
+            ) = party.getProposalStateInfo(proposalId);
+
+            require(
+                status == PartyGovernance.ProposalStatus.Ready &&
+                    proposal.executedTime == block.timestamp,
+                "Wrong proposal ID"
+            );
+
+            proposedTime = proposal.proposedTime;
+            totalVotingPower = party.getGovernanceValues().totalVotingPower;
+        }
+
         address prevMember;
         uint96 totalIntrinsicVotingPower;
-        uint96 totalVotingPower = party.getGovernanceValues().totalVotingPower;
         for (uint256 i = 0; i < members.length; i++) {
             address member = members[i];
 
@@ -56,18 +88,14 @@ contract PushDistributor {
 
             prevMember = member;
 
-            uint96 intrinsicVotingPower = party.getIntrinsicVotingPowerAt(
-                member,
-                block.timestamp,
-                0
-            );
+            uint96 intrinsicVotingPower = party.getIntrinsicVotingPowerAt(member, proposedTime, 0);
 
             totalIntrinsicVotingPower += intrinsicVotingPower;
 
-            uint256 share = ((intrinsicVotingPower) * 1e18) / totalVotingPower;
-
             // We round up here to prevent dust amounts getting trapped in this contract.
-            uint256 shareAmount = (share * amount + (1e18 - 1)) / 1e18;
+            uint256 shareAmount = ((((intrinsicVotingPower) * 1e18) / totalVotingPower) *
+                amount +
+                (1e18 - 1)) / 1e18;
 
             if (shareAmount > 0) {
                 // Transfer the share of the distribution to the member.
